abb2f731bb6deb15dd8148819b82b1eff3c2e1a5
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-05-11 12:21:45 +0200

    change th with route and form for is-available

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -6,14 +6,17 @@
         <table class="table table-striped">
             <thead>
                 <tr>
-                    <th>Casa editrice</th>
-                    <th>Titolo</th>
-                    <th>Autore</th>
-                    <th>Numero pagine</th>
-                    <th>Cod. ISBN</th>
-                    <th>Genere</th>
-                    <th>Disponibile</th>
-                    <th>Azioni</th>
+                    <th><a class="btn btn-link" href="{{ route('books.index', 'sort=publishing_company ') }}">Casa
+                            Editrice</a> </th>
+                    <th><a class="btn btn-link" href="{{ route('books.index', 'sort=title') }}">Titolo</a> </th>
+                    <th><a class="btn btn-link" href="{{ route('books.index', 'sort=author') }}">Autore</a> </th>
+                    <th><a class="btn btn-link" href="{{ route('books.index', 'sort=pages ') }}">Pagine</a> </th>
+                    <th><a class="btn btn-link" href="{{ route('books.index', 'sort=ISBN') }}">Codice ISBN</a> </th>`
+                    <th><a class="btn btn-link" href="{{ route('books.index', 'sort=genre') }}">Genere
+                        </a> </th>
+
+                    <th><a class="btn btn-link" href="{{ route('books.index', 'sort=is_available') }}">Disponibile</a> </th>
+                    <th>Azioni </th>
                 </tr>
             </thead>
             <tbody>
@@ -37,24 +40,41 @@
                         <td>{{ $book->ISBN }}</td>
                         <td>{{ $book->genre ? $book->genre->name : '-' }}</td>
                         <td>
-                            @if ($book->is_available)
+                            {{-- @if ($book->is_available)
                                 Si
                             @else
                                 No
-                            @endif
+                            @endif --}}
+
+
+                            {{-- metodo per cambiare direttamente dalla index la variabile di is_available --}}
+                            <form action="{{ route('books.toggle', $book->id) }}" method="POST">
+                                @method('PATCH')
+                                @csrf
+                                <button type="submit" title="{{ $book->is_available ? 'stock' : 'soldout' }}"
+                                    class="btn btn-outline"> <i
+                                        class="fa-2x fa-solid fas fa-fw {{ $book->is_available ? 'fa-toggle-on' : 'fa-toggle-off' }}"></i>
+                                </button>
+                            </form>
 
                         </td>
-                        <td class="d-flex gap-2">
-                            <a class="btn btn-primary btn-sm" href="{{ route('books.edit', $book->id) }}">
-                                Edit
-                            </a>
-                            <form method="POST" action="{{ route('books.destroy', $book->id) }}">
-                                @csrf
+                        <td class="text-end">
+                            <div class="d-flex gap-2">
+                                <a class=" btn btn-primary" href="{{ route('books.edit', $book->id) }}">
+                                    Edit
+                                </a>
 
-                                @method('DELETE')
+                                <form class="d-inline" method="POST" action="{{ route('books.destroy', $book->id) }}">
+                                    @csrf
 
-                                <input class="btn btn-danger btn-sm" type="submit" value="delete">
-                            </form>
+                                    @method('DELETE')
+
+                                    <input class="btn btn-danger" type="submit" value="Delete">
+                                </form>
+
+                            </div>
+
+
 
                         </td>
                     </tr>
